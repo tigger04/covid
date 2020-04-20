47c79a6b6c0896c159e5e51b51b19f297ef7c3f1
Tadhg O'Brien <tigger04@gmail.com>
2020-04-20 09:44:58 +0100

    update to allow number of entries, shell compatibility

--- covid.sh
+++ covid.sh
@@ -1,6 +1,14 @@
-#!/bin/zsh
+#!/usr/bin/env sh
+
+_covid_nhead=25
+
+if [ $# -ne 0 ] && [ $1 -gt 0 ]
+then
+        _covid_nhead=$1
+fi
+
 curl -s https://coronavirus-19-api.herokuapp.com/countries | \
-  jq -r '(map(keys) | add | unique) as $cols | map(. as $row | $cols | map($row[.])) as $rows | $cols, $rows[] | @csv' | \
-  cut -d',' -f4 -f7 | \
-  sort -t',' -k 2nr | \
-  head -n 40 | nl
+  jq -r '(map(keys) | add | unique) as $cols | map(. as $row | $cols | map($row[.])) as $rows | $cols, $rows[] | @csv'  | \
+          cut -d, -f4,7 | \
+                  sort -t, -k 2nr | \
+                          head -n $_covid_nhead | nl
